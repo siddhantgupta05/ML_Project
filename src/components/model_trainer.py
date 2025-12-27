458950cf480cbdf3ad84bc6c83ecf5607de27f45
siddhantgupta05 <guptasiddhant52@gmail.com>
2025-12-27 19:23:08 +0530

    Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -3,34 +3,39 @@ import sys
 from dataclasses import dataclass
 
 from catboost import CatBoostRegressor
-from sklearn.ensemble import (AdaBoostRegressor, GradientBoostingRegressor, RandomForestRegressor)
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object
-from src.utils import evaluate_models   
+
+from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path=os.path.join('artifacts', "model.pkl")
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
 
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
 
-    def initiate_model_trainer(self, train_array, test_array):
+
+    def initiate_model_trainer(self,train_array,test_array):
         try:
-            logging.info("Splitting Training and Test Input Data")
-            X_train, y_train, X_test, y_test=(
+            logging.info("Split training and test input data")
+            X_train,y_train,X_test,y_test=(
                 train_array[:,:-1],
                 train_array[:,-1],
                 test_array[:,:-1],
                 test_array[:,-1]
-
             )
             models = {
                 "Random Forest": RandomForestRegressor(),
@@ -94,7 +99,7 @@ class ModelTrainer:
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
-            logging.info(f"Best found model on both Training and Testing dataset")
+            logging.info(f"Best found model on both training and testing dataset")
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
@@ -107,5 +112,8 @@ class ModelTrainer:
             return r2_square
             
 
+
+
+            
         except Exception as e:
             raise CustomException(e,sys)
